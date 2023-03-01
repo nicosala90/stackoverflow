24249ba18c8d6b6080a3bd48319af766bb04345b
Beata Reka Bujdoso <beata.reka.bujdoso@codecool.com>
2023-03-01 23:51:50 +0100

    add answers display to question detail page

--- src/main/resources/static/js/html_factory.js
+++ src/main/resources/static/js/html_factory.js
@@ -1,17 +1,17 @@
 export let html_factory = {
-    createTableContent: function (dataList, keylist) {
+    createTableContent: function (dataList, keyList, idKey) {
         const tbody = document.createElement('tbody');
         for (let item of dataList) {
             let itemRow = document.createElement('tr');
-            itemRow.id = item['question_id'];
+            itemRow.id = item[idKey];
             itemRow.classList.add('item');
-            for (let key of keylist) {
+            for (let key of keyList) {
                 let keyTd = itemRow.insertCell();
                 keyTd.textContent = item[key];
             }
             let deleteTd = itemRow.insertCell();
             deleteTd.classList.add('center');
-            deleteTd.innerHTML = `<i class="icon black fas fa-trash" data-id="${item['question_id']}"></i>`
+            deleteTd.innerHTML = `<i class="icon black fas fa-trash" data-id="${item[idKey]}"></i>`
             tbody.insertAdjacentElement('beforeend', itemRow);
         }
         return tbody;
