9ed54f3e2abb8419572bf67cea81822472119fe6
nicosala90 <nicosala90@gmail.com>
2023-03-01 18:30:09 +0100

    get all user, and add new user feature created

---
+++ src/main/java/com/codecool/stackoverflowtw/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.codecool.stackoverflowtw.controller;
+
+
+import com.codecool.stackoverflowtw.controller.dto.NewUserDTO;
+import com.codecool.stackoverflowtw.controller.dto.UserDTO;
+import com.codecool.stackoverflowtw.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/users")
+public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/")
+    public void addNewUser(@RequestBody NewUserDTO user){
+        userService.addNewUser(user);
+    }
+}
